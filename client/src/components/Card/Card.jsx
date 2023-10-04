c3822c73a84f9dbc6e3855df6bdb6bda06616dc9
mariomarquesto <mariomarquesto@gmail.com>
2023-10-04 12:14:35 -0300

    subiendo comentarios a cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,21 +2,21 @@ import { NavLink } from 'react-router-dom';
 import styles from './Card.module.css';
 
 const Card = (props) => {
-  const { id, nombre, Imagendelabandera, continente } = props;
+  const { id, nombre, Imagendelabandera, continente, poblacion } = props;
   return (
-    <>
-      <NavLink className={styles.navlink} to={`/countries/${id}`}>
-        <div className={styles.card}>
-          <img
-            className={styles.cardImg}
-            src={Imagendelabandera}
-            alt={`bandera de ${nombre}`}
-          />
-          <h2 className={styles.cardName}>{nombre}</h2>
-          <h3 className={styles.cardContinent}>{continente}</h3>
-        </div>
-      </NavLink>
-    </>
+
+    <NavLink className={styles.navlink} to={`/countries/${id}`}>
+      <div className={styles.card}>
+        <img
+          className={styles.cardImg}
+          src={Imagendelabandera}
+          alt={`bandera de ${nombre}`}
+        />
+        <h2 className={styles.cardName}>{nombre}</h2>
+        <h3 className={styles.cardContinent}>{continente}</h3>
+      </div>
+    </NavLink>
+
   );
 };
 
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -25,18 +25,23 @@ const Cards = () => {
   // Calcular el índice del primer y último país en la página actual
   const indexOfLastCountry = currentPage * itemsPerPage;
   const indexOfFirstCountry = indexOfLastCountry - itemsPerPage;
+  // Obtener la vista de países para la página actual
   const viewCountries = countries.slice(
     indexOfFirstCountry,
     indexOfLastCountry
   );
 
   useEffect(() => {
+    // Actualizar la lista de países cuando cambia la lista global
+
     setCountries(countriesGlobal);
   }, [countriesGlobal]);
 
   useEffect(() => {
+    // Actualizar la lista de países cuando cambian los países filtrados
+
     setCountries(filterCountries);
-    setCurrentPage(1);
+    setCurrentPage(1); // Volver a la primera página cuando cambian los filtros
   }, [filterCountries]);
 
   const dispatch = useDispatch();
@@ -49,49 +54,71 @@ const Cards = () => {
   const goToNextPage = () => {
     setCurrentPage((prevPage) => prevPage + 1);
   };
-
+  // Función para filtrar por continente
   const filterByContinent = (e) => {
+    // Enviar una acción Redux llamada "continentFilter" con el valor seleccionado
     dispatch(continentFilter(e.target.value));
 
+    // Verificar si se seleccionó "All" en el menú desplegable
     if (e.target.value === 'All') {
       setCountries([...countries]);
     } else {
+      // Establecer la lista de países en "filterCountries" (filtrados por continente)
       setCountries([...filterCountries]);
     }
+    // Reiniciar el valor seleccionado en el menú desplegable
+
     e.target.value = '';
   };
 
+  // Función para ordenar por nombre
   const orderName = (e) => {
+    // Enviar una acción Redux llamada "orderByName" con el valor seleccionado
     dispatch(orderByName(e.target.value));
+    // Reiniciar el valor seleccionado en el menú desplegable de ordenamiento por nombre
     e.target.value = '';
   };
-
+  // Función para ordenar por población
   const orderPopulation = (e) => {
+    // Enviar una acción Redux llamada "orderByPopulation" con el valor seleccionado
     dispatch(orderByPopulation(e.target.value));
+    // Reiniciar el valor seleccionado en el menú desplegable de ordenamiento por población
     e.target.value = '';
   };
 
+  // Función para filtrar por actividad
   const filterByActivity = (e) => {
+    // Enviar una acción Redux llamada "activityFilter" con el valor seleccionado
     dispatch(activityFilter(e.target.value));
+    // Verificar si se seleccionó "All" en el menú desplegable
     if (e.target.value === 'All') {
+      // Restaurar la lista de países original "countries"
       setCountries([...countries]);
     } else {
+      // Establecer la lista de países en "filterCountries" (filtrados por actividad)
       setCountries([...filterCountries]);
     }
+    // Reiniciar el valor seleccionado en el menú desplegable
     e.target.value = '';
   };
 
+  // Declaración de la variable "newActivities" 
   let newAcitivities;
-
+  // Verificar si "activities" es un array antes de filtrar duplicados
   if (Array.isArray(activities)) {
+    // Filtrar actividades duplicadas
     newAcitivities = activities.filter(
       (obj, index, arr) =>
         index === arr.findIndex((t) => t.Nombre === obj.Nombre)
     );
   }
 
+
+  // Función para realizar una búsqueda de países por nombre
   const searchCountry = (name) => {
+    // Enviar una acción Redux llamada "getCountryByName" 
     dispatch(getCountryByName(name));
+    // Establecer la lista de países en "filterCountries" (filtrados) en el estado local
     setCountries([...filterCountries]);
   };
 
@@ -102,6 +129,7 @@ const Cards = () => {
           <SearchBar searchCountry={searchCountry} />
         </div>
         <div className={style.filters}>
+          {/* Menú desplegable para seleccionar un continente */}
           <select className={style.selects} onChange={filterByContinent}>
             <option value="" hidden>
               Continent
@@ -114,7 +142,7 @@ const Cards = () => {
             <option value="Europe">Europe</option>
             <option value="Oceania">Oceania</option>
           </select>
-
+          {/* Menú desplegable para ordenar por nombre */}
           <select className={style.selects} onChange={orderName}>
             <option value="" hidden>
               Order
@@ -122,7 +150,7 @@ const Cards = () => {
             <option value="Ascendente">A - Z</option>
             <option value="Descendente">Z - A</option>
           </select>
-
+          {/* Menú desplegable para ordenar por población */}
           <select className={style.selects} onChange={orderPopulation}>
             <option value="" hidden>
               Population
@@ -130,7 +158,7 @@ const Cards = () => {
             <option value="Ascendente">More population</option>
             <option value="Descendente">Less population</option>
           </select>
-
+          {/* Menú desplegable para seleccionar una actividad */}
           <select
             className={style.selects}
             name="activity"
@@ -167,6 +195,7 @@ const Cards = () => {
             nombre={country.Nombre}
             continente={country.Continente}
             Imagendelabandera={country.Imagendelabandera}
+            Poblacion={country.Poblacion}
           />
         ))}
 
